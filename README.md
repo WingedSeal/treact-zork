6d53935e90ea02ae0275dd30c65e4be0a5f1e437
Kasidit Sarnrak <otto.ksarnrak@gmail.com>
2025-08-29 10:38:04 +0700

    feat: Implement React framework using Langgraph

--- README.md
+++ README.md
@@ -32,3 +32,25 @@ curl -X POST "http://127.0.0.1:8000/zork/zork285" \
   -H "Content-Type: application/json" \
   -d '{"commands": ["look", "inventory", "go east", "go north"]}'
 ```
+
+### How to setup
+
+1. cd mcp-server
+2. pip install uv
+3. uv venv. venv
+4. uv add -r requirements.txt
+5. uv sync
+
+### How to run
+
+1. Open the first terminal
+
+- uv run zork-tool\src\main.py
+
+2. Open the second terminal
+
+- uv run mcp-server\src\server.py
+
+3. Open the third terminal
+
+- uv run mcp-client\src\client.py
--- mcp-client/src/client.py
+++ mcp-client/src/client.py
@@ -17,6 +17,10 @@ from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_ollama import ChatOllama
 from typing import Any
+import orjson
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.graph import StateGraph, START, END
+from typing_extensions import TypedDict
 
 
 load_dotenv("./mcp-client/.env")
@@ -60,6 +64,15 @@ class response(BaseModel):
     ]
 
 
+class State(TypedDict):
+    structured_response: Any
+    history: list[Any] = []
+    llm: Any
+    tool_calls: list[Any] = []
+    current_step: int = 0
+    maximum_step: int = 50
+
+
 class MCPClient:
     def __init__(self):
         # Initialize session and client objects
@@ -87,44 +100,157 @@ class MCPClient:
         print("Connected to server with tools:", [tool.name for tool in tool_results])
         self.tools = tool_results
 
-    async def talk_with_zork(self, history: list[str], llm: Any):
-
-        template = """
-        You are playing Zork, a text adventure game. Your goal is to collect as many treasures as possible.
-
-        ## YOUR REQUIRED STEPS ##
-        STEP 1: Start the game by calling zork-285-api-gen-key tool to get the generated key
-        STEP 2: Use the generated key with the proper command as parameters for zork-285-api-use-key tool
-        STEP 3: Read the game response
-        STEP 4: Understand the current scenarion and come up with a new command
-        STEP 5: Call Zork tool again with the new command and the new generated key
-        STEP 6: Repeat steps 2-5 for 20 times, then proceed to quit the game with the current status
+        async def llm_call(state: State):
+            llm = state["llm"]
+            llm_with_tools = llm.bind_tools(self.tools)
+            template = """
         
-        ## How to track current score ##
-        - Input command 'score' to get the current score
-        
-        ## How to quit the game ##
-        - Input command 'quit' to get the current score and confirmation whether to quit the game
-        
-        ### Important ###
-        - The first command should be 'help' to understand the game mechanics and available commands.
-        - Keep tracking the game state and your inventory and also history commands
-        - Keep tracking the score and current steps
-        - After 20 steps of calling tool, proceed to quit the game with the current status
+            You are playing Zork, a text adventure game. Your goal is to collect as many treasures as possible.
 
-        """
-        agent = create_react_agent(
-            model=llm,
-            tools=self.tools,
-            prompt=template,
-            response_format=response,
-            debug=True,
+            ### Current History of tool calls and output ###
+            {history}
+
+            ## YOUR REQUIRED STEPS ##
+            STEP 1: Start the game by calling zork-285-api-gen-key tool to get the generated key
+            STEP 2: Use the generated key with the proper command as parameters for zork-285-api-use-key tool
+            STEP 3: Read the game response
+            STEP 4: Understand the current scenarion and come up with a new command
+            STEP 5: Call Zork tool again with the new command and the new generated key
+            STEP 6: Repeat steps 2-5 for {maximum_step} times, then proceed to quit the game with the current status
+            
+            ## How to track current score ##
+            - Input command 'score' to get the current score
+            
+            ## How to quit the game ##
+            - Input command 'quit' to get the current score and confirmation whether to quit the game by calling tool
+            
+            ### Important ###
+            - The first command should be 'help' to understand the game mechanics and available commands.
+            - Keep tracking the game state and your inventory and also history commands
+            - Keep tracking the score and current steps
+            - when the current step : {current_step} equals to the maximum number of steps: {maximum_step}, you must quit the game
+
+            """
+            prompt = ChatPromptTemplate.from_template(template)
+            chain = prompt | llm_with_tools
+            result = await chain.ainvoke(
+                {
+                    "history": state["history"],
+                    "current_step": state["current_step"],
+                    "maximum_step": state["maximum_step"] - 3,
+                }
+            )
+            count = state["current_step"] + 1
+            print(f"Count: {count}")
+            pprint.pp(result.tool_calls)
+            return {
+                "tool_calls": result.tool_calls,
+                "current_step": count,
+            }
+
+        def should_continue(state: State):
+            if state["current_step"] > state["maximum_step"]:
+                pprint.pp(f"Reach Maximum")
+                return "END"
+            if state["tool_calls"]:
+                pprint.pp("Action")
+                return "Action"
+            else:
+                return "END"
+
+        async def call_tools(state: State):
+            total_result = []
+            try:
+                for tool in state["tool_calls"]:
+                    result = await self.session.call_tool(
+                        tool["name"], arguments=tool["args"]
+                    )
+                    pprint.pp(result)
+                    final_result = orjson.loads(result.content[0].text)
+                    total_result.append(final_result)
+            except Exception as e:
+                total_result.append({"response": f"Error: {str(e)}"})
+            total_result = state["history"] + total_result
+            return {"history": total_result}
+
+        async def summarize(state: State):
+            llm_with_structured = state["llm"].with_structured_output(response)
+            try:
+                result = await llm_with_structured.ainvoke(
+                    [HumanMessage(content=state["history"])]
+                )
+                return {"structured_response": result}
+            except Exception as e:
+                return {"structured_response": f"Error: {str(e)}"}
+
+        graph = StateGraph(State)
+        graph.add_node("llm_call", llm_call)
+        graph.add_node("environment", call_tools)
+        graph.add_node("summarize", summarize)
+
+        graph.add_edge(START, "llm_call")
+        graph.add_conditional_edges(
+            "llm_call",
+            should_continue,
+            {"Action": "environment", "END": "summarize"},
         )
+        graph.add_edge("environment", "llm_call")
+        graph.add_edge("summarize", END)
+        agent = graph.compile()
+        self.agent = agent
 
-        agent_response = await agent.ainvoke(
-            input={"messages": str(history)},
-            config={"recursion_limit": 5000},
-        )
+    async def talk_with_zork(self, history: list[str], llm: Any, category: str):
+        match category:
+            case "react_framework":
+                template = """
+                You are playing Zork, a text adventure game. Your goal is to collect as many treasures as possible.
+
+                ## YOUR REQUIRED STEPS ##
+                STEP 1: Start the game by calling zork-285-api-gen-key tool to get the generated key
+                STEP 2: Use the generated key with the proper command as parameters for zork-285-api-use-key tool
+                STEP 3: Read the game response
+                STEP 4: Understand the current scenarion and come up with a new command
+                STEP 5: Call Zork tool again with the new command and the new generated key
+                STEP 6: Repeat steps 2-5 for 20 times, then proceed to quit the game with the current status
+                
+                ## How to track current score ##
+                - Input command 'score' to get the current score
+                
+                ## How to quit the game ##
+                - Input command 'quit' to get the current score and confirmation whether to quit the game
+                
+                ### Important ###
+                - The first command should be 'help' to understand the game mechanics and available commands.
+                - Keep tracking the game state and your inventory and also history commands
+                - Keep tracking the score and current steps
+                - After 20 steps of calling tool, proceed to quit the game with the current status
+
+                """
+                agent = create_react_agent(
+                    model=llm,
+                    tools=self.tools,
+                    prompt=template,
+                    response_format=response,
+                    debug=True,
+                )
+
+                agent_response = await agent.ainvoke(
+                    input={"messages": str(history)},
+                    config={"recursion_limit": 100},
+                )
+            case "react_implement":
+                agent_response = await self.agent.ainvoke(
+                    {
+                        "structured_response": None,
+                        "history": [],
+                        "llm": llm,
+                        "tool_calls": [],
+                        "current_step": 0,
+                        "maximum_step": 200,
+                    },
+                    config={"recursion_limit": 300},
+                )
+
         return agent_response
 
     async def cleanup(self):
@@ -158,12 +284,16 @@ client = MCPClient()
 
 async def main():
     await client.connect_to_server()
-
-    result = await client.talk_with_zork(history=[], llm=gemini)
-    pprint.pp(result["structured_response"])
-    print(result["structured_response"].game_completed)
-    print(result["structured_response"].current_status)
-    print(result["structured_response"].score)
+    try:
+        result = await client.talk_with_zork(
+            history=[], llm=gemini, category="react_implement"
+        )
+        pprint.pp(result["structured_response"])
+        print(result["structured_response"].game_completed)
+        print(result["structured_response"].current_status)
+        print(result["structured_response"].score)
+    except Exception as e:
+        print(str(e))
 
     await client.cleanup()
     return
